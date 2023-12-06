0836db0ff32f081ec993c92fc570f498e7a6f768
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-12-06 17:15:01 +0100

    lint

--- pages/floating-items.vue
+++ pages/floating-items.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="relative flex flex-1 items-center justify-center overflow-hidden bg-blue-700 p-4">
     <h1 class="z-20 mx-auto text-center text-7xl font-bold text-yellow-400 md:text-9xl">Floating items title</h1>
-    <div class="pointer-events-none absolute aspect-square w-48 md:w-80 " v-for="ring in rings" :key="ring" :class="ring.classes">
+    <div v-for="ring in rings" :key="ring" class="pointer-events-none absolute aspect-square w-48 md:w-80 " :class="ring.classes">
       <TresCanvas
         preset="realistic"
         :alpha="true"
@@ -11,14 +11,13 @@
           :look-at="[0, 0, 0]"
         />
         <Levioso rotation-factor="2" float-factor="2">
-
           <TresMesh
-          :scale="1"
+            :scale="1"
           >
-          <TresTorusGeometry :args="[1, 0.1, 32, 128]" />
-          <MeshGlassMaterial :color="ring.color" />
-        </TresMesh>
-      </Levioso>
+            <TresTorusGeometry :args="[1, 0.1, 32, 128]" />
+            <MeshGlassMaterial :color="ring.color" />
+          </TresMesh>
+        </Levioso>
         <TresDirectionalLight color="#fff" :intensity="0.5" />
         <TresAmbientLight :intensity="0.1" />
       </TresCanvas>
@@ -27,7 +26,6 @@
 </template>
 
 <script setup>
-
 const rings = ref([
   { color: '#b0b', classes: 'z-10 -translate-x-32 md:-translate-x-80' },
   { color: '#bb0', classes: 'z-30 translate-x-32 md:translate-x-80' },
