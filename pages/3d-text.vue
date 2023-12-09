c793eba5e7a3c862d3ccb864403f31e6e0970e68
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-12-10 00:27:11 +0100

    fix 3d text

--- pages/3d-text.vue
+++ pages/3d-text.vue
@@ -1,25 +1,47 @@
 <template>
-  <main class="flex-1">
-    <TresCanvas
-      clear-color="#facc15"
-      preset="realistic"
-    >
-      <TresPerspectiveCamera
-        :position="[3, 3, 3]"
-        :look-at="[0, 0, 0]"
-      />
-      <Text3D
-        text="My 3D Text"
-        font="/fonts/Inter_Bold.json"
-      >
-        <TresMeshPhongMaterial color="#8b5cf6" />
-      </Text3D>
-      <TresDirectionalLight color="#fff" :intensity="0.5" />
-      <TresAmbientLight :intensity="0.1" />
-    </TresCanvas>
+  <main class="flex flex-1  items-center justify-center overflow-hidden bg-slate-200">
+    <div class="mx-auto grid aspect-square max-h-[80%]  min-w-full grid-cols-4 grid-rows-4 md:min-w-[40%]">
+      <div v-for="letter in letters" :key="letter" class=" col-span-2 aspect-square" :class="letter.classes">
+        <TresCanvas
+          preset="realistic"
+          :alpha="true"
+        >
+          <TresPerspectiveCamera
+            :position="[3, 3, 3]"
+            :look-at="[0, 0, 0]"
+          />
+          <Levioso>
+            <Suspense>
+              <Text3D
+                :text="letter.char"
+                font="/fonts/Inter_Bold.json"
+                size="2"
+                bevel-size="0.04"
+                bevel-thickness="0.1"
+              >
+                <TresMeshPhongMaterial :color="letter.color" />
+              </Text3D>
+            </Suspense>
+          </Levioso>
+          <TresDirectionalLight color="#fff" :intensity="1" />
+          <TresAmbientLight :intensity="0.5" />
+        </TresCanvas>
+      </div>
+    </div>
   </main>
 </template>
 
 <script setup>
+const letters = ref([
+  { char: 'X', color: '#3b82f6', classes: 'row-start-1 col-start-2' },
+  { char: 'Y', color: '#10b981', classes: 'row-start-2 col-start-1' },
+  { char: 'A', color: '#ef4444', classes: 'row-start-2 col-start-3' },
+  { char: 'B', color: '#facc15', classes: 'row-start-3 col-start-2' },
 
+])
 </script>
+
+<style scoped>
+
+</style>
+```
