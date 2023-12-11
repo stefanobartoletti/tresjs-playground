b85d670e7c79bf4753906900e12392d0f93b3f4c
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-12-11 11:12:37 +0100

    fix 3d text

--- pages/3d-text.vue
+++ pages/3d-text.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-1  items-center justify-center overflow-hidden bg-slate-200">
+  <main class="flex flex-1  items-center justify-center overflow-hidden bg-slate-800">
     <div class="mx-auto grid aspect-square max-h-[80%]  min-w-full grid-cols-4 grid-rows-4 md:min-w-[40%]">
       <div v-for="letter in letters" :key="letter" class=" col-span-2 aspect-square" :class="letter.classes">
         <TresCanvas
@@ -18,12 +18,23 @@
                 size="2"
                 bevel-size="0.04"
                 bevel-thickness="0.1"
+                curve-segments="12"
+                bevel-segments="12"
               >
-                <TresMeshPhongMaterial :color="letter.color" />
+                <TresMeshPhysicalMaterial
+                  :color="letter.color"
+                  roughness="0.5"
+                  metalness="0.5"
+                  ior="1.5"
+                />
               </Text3D>
             </Suspense>
           </Levioso>
-          <TresDirectionalLight color="#fff" :intensity="1" />
+          <TresDirectionalLight
+            color="#fff"
+            :intensity="1"
+            :position="[1, 3, 1]"
+          />
           <TresAmbientLight :intensity="0.5" />
         </TresCanvas>
       </div>
@@ -33,10 +44,10 @@
 
 <script setup>
 const letters = ref([
-  { char: 'X', color: '#3b82f6', classes: 'row-start-1 col-start-2' },
-  { char: 'Y', color: '#10b981', classes: 'row-start-2 col-start-1' },
-  { char: 'A', color: '#ef4444', classes: 'row-start-2 col-start-3' },
-  { char: 'B', color: '#facc15', classes: 'row-start-3 col-start-2' },
+  { char: 'Y', color: '#facc15', classes: 'row-start-1 col-start-2' },
+  { char: 'X', color: '#3b82f6', classes: 'row-start-2 col-start-1' },
+  { char: 'B', color: '#ef4444', classes: 'row-start-2 col-start-3' },
+  { char: 'A', color: '#10b981', classes: 'row-start-3 col-start-2' },
 
 ])
 </script>
