0d4f4d5be5bd23bd292cd0fb2491708cc2fed59c
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-04-03 10:09:32 +0200

    add error message

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SiteHeader />
+    <div class="flex h-screen flex-col items-center justify-center gap-4 border">
+      <p class="h1">{{ error.statusCode }}</p>
+      <p>{{ error.message }}</p>
+      <button class="btn" @click="handleError">Clear errors</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const error = useError()
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
