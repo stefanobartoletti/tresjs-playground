1bd844fdc8173ee521390267e4c1aef5b6bf5fbb
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-10-03 12:42:58 +0200

    add homecard

---
+++ components/HomeCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLink :to="`/${item}`">
+    <div class="card bg-base-100 shadow-xl">
+      <figure>
+        <img :src="`/img/${item}.png`" :alt="useKebabToCapital(item)" />
+      </figure>
+      <div class="card-body px-6 py-4">
+        <span class="card-title">{{ useKebabToCapital(item) }}</span>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+  item: { type: Array, required: true },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,7 @@
 <template>
   <main class="mt-20 flex-1 p-4">
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <NuxtLink v-for="item in menu" :key="item" :to="`/${item}`">
-        <div class="card bg-base-100 shadow-xl">
-          <figure>
-            <img :src="`/img/${item}.png`" :alt="useKebabToCapital(item)" />
-          </figure>
-          <div class="card-body px-6 py-4">
-            <span class="card-title">{{ useKebabToCapital(item) }}</span>
-          </div>
-        </div>
-      </NuxtLink>
+      <HomeCard v-for="item in menu" :key="item" :item="item" />
     </div>
   </main>
 </template>
